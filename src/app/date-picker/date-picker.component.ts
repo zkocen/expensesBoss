dfe7261b2c2906bbe94dbf1c407ad439f4f8f8a4
Zan Kocen <zkocen@gmail.com>
2020-05-05 12:49:07 +0100

    date ready

--- src/app/date-picker/date-picker.component.ts
+++ src/app/date-picker/date-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import {
   MomentDateAdapter,
@@ -11,6 +11,7 @@ import {
 } from '@angular/material/core';
 import { MatDatepicker } from '@angular/material/datepicker';
 import * as moment from 'moment';
+import { Moment } from 'moment';
 
 // See the Moment.js docs for the meaning of these formats:
 // https://momentjs.com/docs/#/displaying/format/
@@ -40,23 +41,19 @@ export const MY_FORMATS = {
     { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
   ],
 })
-export class DatePickerComponent implements OnInit {
-  date = new FormControl(moment());
-  ngOnInit(): void {}
+export class DatePickerComponent implements OnInit, OnChanges {
+  public date = new FormControl(moment());
+  public currentMonth: string;
 
-  chosenYearHandler(normalizedYear: moment.Moment) {
-    const ctrlValue = this.date.value;
-    ctrlValue.year(normalizedYear.year());
-    this.date.setValue(ctrlValue);
-  }
+  public ngOnInit() {}
 
-  chosenMonthHandler(
-    normalizedMonth: moment.Moment,
-    datepicker: MatDatepicker<moment.Moment>
-  ) {
+  public ngOnChanges() {}
+
+  chosenMonthHandler(selectedMonth: Moment, datepicker: MatDatepicker<Moment>) {
     const ctrlValue = this.date.value;
-    ctrlValue.month(normalizedMonth.month());
+    ctrlValue.month(selectedMonth.month());
     this.date.setValue(ctrlValue);
+    this.currentMonth = selectedMonth.toISOString();
     datepicker.close();
   }
 }
