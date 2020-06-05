4ceab3e2491f6de9c5c972bbc68fc34db2e829d5
Zan Kocen <zkocen@gmail.com>
2020-06-05 14:31:27 +0100

    url

--- src/app/shared/baseUrl.ts
+++ src/app/shared/baseUrl.ts
@@ -1,3 +1,1 @@
-export const baseUrl =
-  'http://localhost:8080/api/' ||
-  'https://limitless-shore-40407.herokuapp.com/api/';
+export const baseUrl = 'https://limitless-shore-40407.herokuapp.com/api/';
