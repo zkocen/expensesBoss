d74d3e3974a41e7224c12b4633edc89e6c580cac
Zan Kocen <zkocen@gmail.com>
2020-06-08 17:15:45 +0100

    active ex this month

--- src/app/charts-dashboard/charts-dashboard.component.ts
+++ src/app/charts-dashboard/charts-dashboard.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AppState } from '../store/app.state';
 import { Store } from '@ngrx/store';
 import { selectedMonth } from '../store/UI/expenses/expenses.selector';
-import { map } from 'rxjs/operators';
+import { map, filter } from 'rxjs/operators';
+import { Expense } from '../store/UI/expenses/expenses.state';
 
 @Component({
   selector: 'app-charts-dashboard',
@@ -15,6 +16,16 @@ export class ChartsDashboardComponent implements OnInit {
       x.expenses.map((ex) => {
         return { name: ex.name, value: ex.amount };
       })
+    )
+  );
+
+  public selectedMonthNotPaid$ = this.store.select(selectedMonth).pipe(
+    map((x) =>
+      x.expenses
+        .filter((y: Expense) => !y.paid)
+        .map((ex) => {
+          return { name: ex.name, value: ex.amount };
+        })
     )
   );
 
